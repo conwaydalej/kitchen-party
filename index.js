b022dd4616d6fa8c935eb4ef0358377c03ec2a64
Dale Conway <conway@mun.ca>
2018-04-03 16:33:31 -0230

    update player tests - add Tone.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
 
-app.get('/player', function(req, res){
-    res.sendFile(__dirname + '/player.html');
-});
+app.use(express.static('public'))
+
 
 io.on('connection', function(socket){
     console.log('a user connected');
